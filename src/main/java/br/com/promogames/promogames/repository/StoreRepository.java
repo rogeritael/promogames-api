05a16cd22d0084b5de3c7cf38c19956246a8cdfc
Roger Rosa <rogeritael.contato@gmail.com>
2026-07-29 20:33:49 -0300

    feature: Configuração do repository da entidade store e implementação da service

---
+++ src/main/java/br/com/promogames/promogames/repository/StoreRepository.java
@@ -0,0 +1,7 @@
+package br.com.promogames.promogames.repository;
+
+import br.com.promogames.promogames.entity.Store;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface StoreRepository extends JpaRepository<Store, Long> {
+}
---
+++ src/main/java/br/com/promogames/promogames/service/StoreService.java
@@ -0,0 +1,27 @@
+package br.com.promogames.promogames.service;
+
+import br.com.promogames.promogames.entity.Store;
+import br.com.promogames.promogames.repository.StoreRepository;
+import lombok.AllArgsConstructor;
+import lombok.NoArgsConstructor;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+@AllArgsConstructor
+public class StoreService {
+    private StoreRepository storeRepository;
+
+    public Store save(Store store){
+        return storeRepository.save(store);
+    }
+
+    public List<Store> findAll(){
+        return storeRepository.findAll();
+    }
+
+    public void delete(Long id){
+        storeRepository.deleteById(id);
+    }
+}
